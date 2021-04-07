fb30376801cefefd29cee5464847df31f2cd4d03
matty21370 <matthewburke01@hotmail.co.uk>
2021-04-07 22:07:18 +0100

    Added and initialized EJS

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
