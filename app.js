3f446d33584ce49022c807882f2575859798fa3c
matty21370 <matthewburke01@hotmail.co.uk>
2021-04-08 20:21:50 +0100

    Added staff functionality

--- app.js
+++ app.js
@@ -29,7 +29,8 @@ mongoose.connect(process.env.DB_CONNECT, {useNewUrlParser: true, useUnifiedTopol
 const userSchema = mongoose.Schema({
     name: String,
     username: String,
-    password: String
+    password: String,
+    role: String
 });
 
 const User = mongoose.model("User", userSchema);
@@ -57,7 +58,8 @@ app.post("/register", (req, res) => {
                 let _user = new User({
                     name: _name,
                     username: _username,
-                    password: hash
+                    password: hash,
+                    role: "Client"
                 });
         
                 _user.save();
@@ -104,7 +106,7 @@ app.post("/login", (req, res) => {
 
 app.get("/main", (req, res) => {
     if(req.session.username) {
-        res.render("main", {ejs_name: req.session.username});
+        res.render("usermain", {ejs_name: req.session.username});
     } else {
         res.redirect("/");
     }
@@ -125,10 +127,42 @@ app.get('/addstaff', (req, res) => {
 });
 
 app.post('/addstaff', (req, res) => {
-    let _password = req.body.pass;
-    if(_password === process.env.ADMIN_PASSWORD) {
-        res.render('secret');
+    if(req.body.adminpass) { //Access page
+        let _password = req.body.adminpass;
+        if(_password === process.env.ADMIN_PASS) {
+            res.render('secret');
+        } else {
+            res.redirect("/addstaff")
+        }
+    } else if(req.body.password) { //Staff sign up page
+        let _name = req.body.name;
+        let _username = req.body.username;
+        let _password = req.body.password;
+
+        if(_name && _username && _password) {
+            bcrypt.hash(_password, saltRounds, (err, hash) => {
+                if(err) {
+                    res.send(err);
+                } else {
+                    let newUser = new User({
+                        name: _name,
+                        username: _username,
+                        password: hash,
+                        role: "Staff"
+                    });
+                    newUser.save();
+                    req.session.username = _username;
+                    res.redirect("/staff");
+                }
+            });
+        } else {
+            res.redirect("/addstaff");
+        }
     }
+});
+
+app.get("/staff", (req, res) => {
+    res.render("staffmain", {ejs_name: req.session.username});
 });
 
 app.listen(3000, () => {
